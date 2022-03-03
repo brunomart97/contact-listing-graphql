76cf27867e26d24a3c0f9828c00c6a4627f35978
Bruno Martins <brunomart97@hotmail.com>
2022-03-03 13:31:54 -0300

    GraphQL methods and writing queries

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,18 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
+
 import AboutTemplate from 'templates/About'
 
-function About() {
+export default function AboutPage() {
   return <AboutTemplate />
 }
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
