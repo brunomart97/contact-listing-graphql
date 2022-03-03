584dda4cc2c1200d723fe490b2e355f53b6eb194
Bruno Martins <brunomart97@hotmail.com>
2022-03-03 10:26:37 -0300

    About page creation

---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+import AboutTemplate from 'templates/About'
+
+function About() {
+  return <AboutTemplate />
+}
+
+export default About
---
+++ src/templates/About/index.tsx
@@ -0,0 +1,28 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as SC from './styles'
+
+const AboutTemplate = () => {
+  return (
+    <SC.Content>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} />
+      </LinkWrapper>
+
+      <SC.Heading>My Trips</SC.Heading>
+
+      <SC.Body>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+          quaerat placeat tempore impedit enim est nemo soluta odio. Quia
+          consequatur fugiat tenetur accusamus aliquid itaque animi, deleniti
+          eos porro. Beatae.
+        </p>
+      </SC.Body>
+    </SC.Content>
+  )
+}
+
+export default AboutTemplate
